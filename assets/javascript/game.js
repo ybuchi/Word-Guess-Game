a5a4b876b0fbbfbca94e78f89518827325345f95
Yannik Buchi <ybuchi@ithaka.org>
2019-08-01 17:50:57 -0400

    Fixed the number of guesses remaining to make it deplete when a user choice is not included in the letterArray

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,13 +15,18 @@
 
     var gameWords = ['football','real madrid', 'champions league', 'bayern munich', 'arsenal'];
     console.log(gameWords);
+    var wordsGuessed = 0;
+    var guessesRemaining = 10;
 
     //Grab the empty div in which we want to add our tick marks
-    var gameTickDiv = document.getElementById("game-letters")
+    var gameTickDiv = document.getElementById("game-letters");
 
+    //Grab the divs containing the scores we want to track
+    var wordsGuessedText = document.getElementById("words-guessed-text");
+    var guessesRemainingText = document.getElementById("guesses-remaining-text");
+    var lettersGuessed = document.getElementById("letters-guessed");
     //Create variables for the scores we want to keep track of
-    var wordsGuessed = 0;
-    var guessesReminaing = 0;
+
 
 
 /////////////////
@@ -29,6 +34,12 @@
     document.onkeyup = function(event) {
 
     //AFTER INITIAL CLICK< THE GAME IS SET UP
+        //The scores are displayed
+        wordsGuessedText.textContent = "# of Words Guessed: " + wordsGuessed;
+        guessesRemainingText.textContent = "# of Guesses Remaining: " + guessesRemaining;
+        lettersGuessed.textContent = "Letters Guessed: " 
+
+
         //The Computer makes a choice
         var compChoice = gameWords[Math.floor(Math.random() * gameWords.length)];
         console.log(compChoice);
@@ -62,7 +73,7 @@
                 }
         }
     
-    //ONCE THE WORD IS SET UP, THE PLAYER CAN START CHOOSING LETTERS
+    //ONCE THE WORD IS SET UP, THE PLAYER CAN START CHOOSING LETTERS AS LONG AS THERE ARE TICKS
 
     //We record the key that the user pressed
         document.onkeyup = function(gamePlay) {
@@ -72,21 +83,22 @@
 
     //If the key pressed is equal to one of the letters in the array, then display the letter. USE ANOTHER FOR LOOP.
     
+    //If the user's choice fits into one of the array values, display the letters in their position
+    //IF THE letter that the user chose is in the array, then run this
+    if (letterArray.includes(userChoice)) {
             for (i=0; i < letterArray.length; i++){
 
                 if (userChoice === letterArray[i]) {
 
                 document.getElementById("tick-id" + [i]).innerHTML = userChoice;
-
-            } else{
-                //If the user guesses wrong, the number of guesses remaning DECREASES, and the letter guessed is displayed
-                wordsGuessed++;
-                guessesReminaing++;
-
-            }
+            } 
         }
-    };
-};
+    }else {
+        guessesRemaining--;
+    }
+    guessesRemainingText.textContent = "# of Guesses Remaining: " + guessesRemaining;
+}   
+}
 
 
 //When the user presses a key
