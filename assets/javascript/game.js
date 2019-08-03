17d55c9a89a367ebf05dcdee30457d2a071acfec
Yannik Buchi <yabuch_ch@hotmail.com>
2019-08-03 11:13:25 -0400

    Bugged fixed so that wrong letter is not repeated.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,6 +15,8 @@
 
     var gameWords = ['football','real madrid', 'champions league', 'bayern munich', 'arsenal'];
     console.log(gameWords);
+
+
     var wordsGuessed = 0;
     var guessesRemaining = 10;
 
@@ -33,11 +35,13 @@
 ///////////////// GAME START : When the user clicks on a key, the game sets up, and then starts
     document.onkeyup = function(event) {
 
-    //AFTER INITIAL CLICK< THE GAME IS SET UP
+    //AFTER INITIAL CLICK< THE GAME IS SET UP, DECLARE VARIABLES
         //The scores are displayed
+
         wordsGuessedText.textContent = "# of Words Guessed: " + wordsGuessed;
         guessesRemainingText.textContent = "# of Guesses Remaining: " + guessesRemaining;
         lettersGuessed.textContent = "Letters Guessed: " 
+
 
 
         //The Computer makes a choice
@@ -81,32 +85,68 @@
             var userChoice = gamePlay.key;
             console.log(userChoice);
 
+
     //If the key pressed is equal to one of the letters in the array, then display the letter. USE ANOTHER FOR LOOP.
     
     //If the user's choice fits into one of the array values, display the letters in their position
     //IF THE letter that the user chose is in the array, then run this, if the letter is NOT in the array, then run the else:
+//______________________________________________________________________________
+    //IF THE USER IS CORRECT:
     if (letterArray.includes(userChoice)) {
             for (i=0; i < letterArray.length; i++){
 
                 if (userChoice === letterArray[i]) {
-
+//Displays the user Choice. 
                 document.getElementById("tick-id" + [i]).innerHTML = userChoice;
             } 
         }
-    }else {
-        guessesRemaining--;
+//______________________________________________________________________________
+    //If THE USER IS INCORRECT- FIrst incorrect answer
+    } else { 
+
         var wrongLetterDiv = document.createElement("div");
+        wrongLetterDiv.setAttribute("id", "wrong-user-choice" + userChoice);
+
+        var letterCheckDiv = document.getElementById("wrong-user-choice" + userChoice);
         wrongLetterDiv.textContent = userChoice;
-        lettersGuessed.appendChild(wrongLetterDiv);
+
+        console.log(document.getElementById("wrong-user-choice" + userChoice));
+        console.log(wrongLetterDiv.getAttribute("id"));
+
+        if (letterCheckDiv === null){
+
+
+            
+            guessesRemaining--;
+            lettersGuessed.appendChild(wrongLetterDiv);
+
+            console.log("I gotchu");
+            console.log(guessesRemaining);
+
+        } 
 
     }
-    guessesRemainingText.textContent = "# of Guesses Remaining: " + guessesRemaining;
+
+    
+
+                //Create the div and give it an id, and display it on the first wrong guess
+
+
+
+
+        }
+
+            //ALL OTHER WRONG GUESSES - check if letter has already been guessed
+    };
+
 
     //If guesses = 0; you loose
     //If word is guessed, move to the next one
+    //If you guess the same word twice, you shouldn't get 
 
-}   
-}
+
+
+
 
 
 //When the user presses a key
